64faf72ddb69f7b7f1d9830a1d37de159edc6fd1
jeffery3472 <jessejeff1489@gmail.com>
2021-12-21 10:10:31 -0800

    update

--- lib/profile.dart
+++ lib/profile.dart
@@ -7,29 +7,45 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.lightBlue.shade50,
+    return Scaffold(
     appBar: AppBar(iconTheme: const IconThemeData(color: Colors.black),backgroundColor: Colors.white,
     title: Padding(
-      padding: const EdgeInsets.only(left:60.0),
+      padding: const EdgeInsets.only(left:130.0),
       child: Text("Profile",style: TextStyle(color: Colors.black),),
     ),
-    actions: [Icon(Icons.more_vert,color: Colors.black,)],
-    bottom: PreferredSize(child: Padding(
-      padding: const EdgeInsets.only(bottom: 50),
-      child: Column(children: [
-        CircleAvatar(backgroundImage: NetworkImage("https://media.istockphoto.com/photos/middle-aged-man-portrait-picture-id1285156699?b=1&k=20&m=1285156699&s=170667a&w=0&h=l8Bs_xHY2um7IUHKXj7szXvNDW5jMJmnLMuvQWO5vEo="),
-        radius: 50,),SizedBox(height: 12,),
-        Text("Jeffery Amoah",style: TextStyle(fontWeight: FontWeight.bold),),
-        Text("Takorodi, Ghana",style: TextStyle(color: Colors.grey),)
-
+    actions: [Icon(Icons.more_vert,color: Colors.black,)],elevation: 0,
+    ),
+    body: ListView(children: [
+       CircleAvatar(backgroundImage: NetworkImage("https://images.unsplash.com/photo-1590086782957-93c06ef21604?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"),
+    radius: 50  ,
+      ),
+    Center(child: Text("Jesse Jeff Amoah",style: TextStyle(fontWeight: FontWeight.bold),)),
+    Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 150),
+      child: Row(children: [
+        Icon(Icons.location_on,color: Colors.grey,),Text("Takoradi Ghana",style: TextStyle(color: Colors.grey),)
       ],),
     ),
-     preferredSize: Size.fromHeight(200)),),
-    body: ListView(children: [
-      Container(child:  ListTile(title: Text("Mobile"),
-      subtitle: Text("+233 545 365 777"),
-      
-      ),)
+    Container(child: ListTile(
+      title: Text("Mobile"),
+      subtitle: Text("+233 546 773 443"),
+      trailing: Row(
+        children: [
+          TextButton(onPressed: (){}, 
+          child: Icon(Icons.message_rounded),
+          style: TextButton.styleFrom(
+            shape: CircleBorder(),
+            backgroundColor: Colors.white
+          ) ),
+           TextButton(onPressed: (){}, 
+          child: Icon(Icons.call),
+          style: TextButton.styleFrom(
+            shape: CircleBorder(),
+            backgroundColor: Colors.white
+          ) ),
+        ],
+      ),
+    ),)
     ],),
       
     );
